01dd939d267768699e48dbac7c5fada034d645f7
scojr <180873402+scojr@users.noreply.github.com>
2024-12-26 15:54:02 -0600

    Initialize Webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+// webpack.config.js
+const { watchFile } = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
