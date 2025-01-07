a0231f9d6b1bb10047ecf3a79184ad98ec5d1597
scojr <180873402+scojr@users.noreply.github.com>
2025-01-06 19:57:41 -0600

    Style card elements

--- src/menu.js
+++ src/menu.js
@@ -13,10 +13,10 @@ const menuEntries = [
   { name: "Spaghetti and Meatballs", description: "Classic spaghetti in a rich marinara sauce, served with hand-rolled meatballs and Parmesan.", img: spaghetti, by: "James/powerplantop", source: "https://www.flickr.com/photos/40726522@N02/14974952510" },
   { name: "Garlic Knots", description: "Golden-baked dough knots brushed with garlic butter, sprinkled with Parmesan, and served with marinara sauce." },
   { name: "Chicken Parm Hero", description: "Crispy breaded chicken cutlet, marinara sauce, and melted mozzarella in a toasted hero roll." },
-  { name: "Fettuccine Alfredo", description: "Smooth, creamy Alfredo sauce tossed with tender fettuccine and finished with fresh parsley." },
   { name: "New York Cheesecake", description: "Rich, creamy cheesecake with a graham cracker crust, topped with a dollop of whipped cream.", img: cheesecake, by: "sabotrax", source: "https://flickr.com/photos/sabotrax/485123910/" },
-  { name: "Cannoli Duo", description: "Crispy pastry shells filled with sweet ricotta cream and chocolate chips, finished with a sprinkle of powdered sugar." },
+  { name: "Fettuccine Alfredo", description: "Smooth, creamy Alfredo sauce tossed with tender fettuccine and finished with fresh parsley." },
   { name: "Classic Cheese Calzone", description: "Golden-baked dough stuffed with ricotta, mozzarella, and Parmesan, served with a side of warm marinara for dipping.." },
+  { name: "Cannoli Duo", description: "Crispy pastry shells filled with sweet ricotta cream and chocolate chips, finished with a sprinkle of powdered sugar." },
   { name: "Chocolate Lava Cake", description: "A decadent chocolate cake with a molten center, served warm with a scoop of vanilla gelato." },
 ];
 
