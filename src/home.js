f41c9e7e9f1f07e4f8568c7e51dc5002441c0259
scojr <180873402+scojr@users.noreply.github.com>
2025-01-08 23:01:58 -0600

    Add image content to home page

--- src/home.js
+++ src/home.js
@@ -1,9 +1,17 @@
 import { bannerConstructor, constructCardGrid } from "./constructors";
-import heroImage from "./images/banner-goncharenok.jpg"
+import heroImage from "./images/banner-goncharenok.jpg";
+import tomatoCard from "./images/home-top5way.jpg";
+import ovenCard from "./images/home-fariphotography.jpg";
+import boxesCard from "./images/home-maxavans.jpg";
+import bannerCard from "./images/home-castillo.jpg";
 
 export function loadHome() {
-  const homeGrid = constructCardGrid(1);
+  const homeGrid = constructCardGrid(2);
+  const homeAbout = homeGrid.newCard("Welcome to Hans Pizzeria", "Step into a slice of New York tradition! At Hans Pizza, we bring you the perfect blend of old-world charm and modern flavors. From our signature hand-tossed pizzas to our hearty pastas and delightful deserts, every dish is crafted with love, fresh ingredients, and a commitment to quality.");
+  homeAbout.scale(2, 0);
   bannerConstructor(heroImage, "Maksim Goncharenok", "https://www.pexels.com/photo/person-holding-a-pizza-4773769/", "Welcome to Hans.", "Authentic flavors, crafted with love.");
-  homeGrid.newCard("Hans Pizzeria", "Welcome to Hans Pizzeria, Manhattan’s slice of heaven! Nestled in the heart of New York City, our pizzeria has been proudly serving the community with authentic, mouthwatering pizza and Italian classics since 2004.");
-  homeGrid.newCard("Lorem ipsum dolor", "Lorem ipsum dolor sit amet, est apeirian mandamus in. No per sale pericula necessitatibus, an ferri delectus pri. Labores blandit cum ut, his cu atqui quodsi, atomorum senserit ex mel. Nam quod posse definitionem no, ex vim magna facilis constituto, eu viris blandit evertitur qui. An quis suas vis, error liberavisse usu an.");
+  const menuCard = homeGrid.newCard("", "", boxesCard, "Max Avans", "https://www.pexels.com/photo/person-slicing-the-pizzas-5056849/");
+  menuCard.scale(0, 2);
+  homeGrid.newCard("", "", ovenCard, "Eneida Nieves", "https://www.pexels.com/photo/baked-pizza-on-pizza-peel-in-oven-905847/");
+  homeGrid.newCard("", "", tomatoCard, "Abdul Kadher Akib", "https://www.pexels.com/photo/close-up-photo-of-red-tomatoes-11463629/");
 }
