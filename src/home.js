eaa0547218b2ef3fb4f36d8303c03367a64f0b18
scojr <180873402+scojr@users.noreply.github.com>
2025-01-10 14:29:10 -0600

    Style banners on home page

--- src/home.js
+++ src/home.js
@@ -9,8 +9,8 @@ export function loadHome() {
   const homeAbout = homeGrid.newCard("Welcome to Hans Pizzeria", "Step into a slice of New York tradition! At Hans Pizza, we bring you the perfect blend of old-world charm and modern flavors. From our signature hand-tossed pizzas to our hearty pastas and delightful deserts, every dish is crafted with love, fresh ingredients, and a commitment to quality.");
   homeAbout.scale(2, 0);
   bannerConstructor(heroImage, "Maksim Goncharenok", "https://www.pexels.com/photo/person-holding-a-pizza-4773769/", "Welcome to Hans.", "Authentic flavors, crafted with love.");
-  const menuCard = homeGrid.newCard("", "", boxesCard, "Max Avans", "https://www.pexels.com/photo/person-slicing-the-pizzas-5056849/");
+  const menuCard = homeGrid.newCard("Dine-in, Pick-up & Delivery", "", boxesCard, "Max Avans", "https://www.pexels.com/photo/person-slicing-the-pizzas-5056849/");
   menuCard.scale(0, 2);
-  homeGrid.newCard("", "", ovenCard, "Eneida Nieves", "https://www.pexels.com/photo/baked-pizza-on-pizza-peel-in-oven-905847/");
-  homeGrid.newCard("", "", tomatoCard, "Abdul Kadher Akib", "https://www.pexels.com/photo/close-up-photo-of-red-tomatoes-11463629/");
+  homeGrid.newCard("Daily Specials", "", ovenCard, "Eneida Nieves", "https://www.pexels.com/photo/baked-pizza-on-pizza-peel-in-oven-905847/");
+  homeGrid.newCard("Fresh Ingredients", "", tomatoCard, "Abdul Kadher Akib", "https://www.pexels.com/photo/close-up-photo-of-red-tomatoes-11463629/");
 }
