afd9b4ed3beccc59c883983cd36c38f3db726375
scojr <180873402+scojr@users.noreply.github.com>
2025-01-10 14:27:28 -0600

    Add content to and style contact page

--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,18 @@
 import { bannerConstructor, constructCardGrid } from "./constructors";
 import contactBanner from "./images/banner-piazza.jpg";
+import contactImage from "./images/contact-cottonbro.jpg";
 
 export function loadContact() {
-  const contactGrid = constructCardGrid(2);
-  bannerConstructor(contactBanner, "Pizzeria Calvino", "https://web.archive.org/web/20161013040320/http://www.panoramio.com/photo/14366314", "Let's Connect!", "We’d Love to Hear from You.");
-  contactGrid.newCard("Hans Pizzeria", "Welcome to Hans Pizzeria, Manhattan’s slice of heaven! Nestled in the heart of New York City, our pizzeria has been proudly serving the community with authentic, mouthwatering pizza and Italian classics since 2004.");
-  contactGrid.newCard("Lorem ipsum dolor", "Lorem ipsum dolor sit amet, est apeirian mandamus in. No per sale pericula necessitatibus, an ferri delectus pri. Labores blandit cum ut, his cu atqui quodsi, atomorum senserit ex mel. Nam quod posse definitionem no, ex vim magna facilis constituto, eu viris blandit evertitur qui. An quis suas vis, error liberavisse usu an.");
+  const contactGrid = constructCardGrid(3);
+  bannerConstructor(contactBanner, "Pizzeria Calvino", "https://web.archive.org/web/20161013040320/http://www.panoramio.com/photo/14366314", "Let's Connect!", "We'd Love to Hear from You.");
+  const hours = contactGrid.newCard("Hours", "Sunday\n10:00 AM - 3:00 AM\nMonday\n10:00 AM - 3:00 AM\nTuesday\n10:00 AM - 3:00 AM\nWednesday\n10:00 AM - 3:00 AM\nThursday\n10:00 AM - 3:00 AM\nFriday\n10:00 AM - 5:00 AM\nSaturday\n10:00 AM - 5:00 AM");
+  hours.scale(0, 3);
+
+  contactGrid.newCard("Address", "1435 Broadway\nNew York, NY 10018");
+
+  const image = contactGrid.newCard("", "", contactImage, "cottonbro studio", "https://www.pexels.com/photo/person-holding-a-pizza-with-cheese-3944312/");
+  image.scale(0, 3);
+  contactGrid.newCard("Phone", "(555) 555-5555");
+  contactGrid.newCard("Email", "han@hanspizza.com");
+
 }
